7313cc749e2f165b51c7f06eeeb8b818afa724f4
Ayo <heyayomideadebisi@gmail.com>
2023-04-10 12:52:35 +0100

    build: test ssg and ssr

--- pages/home.jsx
+++ pages/home.jsx
@@ -3,26 +3,20 @@ import requests from 'lib/requests'
 import Header from '@/components/Header'
 
 // const user = auth.currentUser;
-const Home = () => {  
-  const [data,setData] = useState('')
+const Home = ({data}) => {  
+  // const [blob,setBlob] = useState(data)
+  // console.log((data));
 
-  useEffect(() => {
-      fetch('https://api.themoviedb.org/3/movie/popular?api_key=9b0b141b44e2b1a77e4965a9ac513ca4')
-      .then(Response => Response.json())
-      .then(value => setData(value.results))      
-      // setData(value)
-    },[])
-    console.log(data);
-  
   return (
       <div className="bg-black text-white h-full">
         <Header/>
+        <p>lets goo</p>
         Hello
         <div>
           {
-            data.map(data => (
+            data.map((data,idx) => (
               <>
-              <p key={data.id}>{data.title}</p>
+              <p key={idx}>{data.title}</p>
               </>
             ))
           }
@@ -30,8 +24,26 @@ const Home = () => {
       </div>
   )
 }
-
 export default Home
+
+// export async function getServerSideProps(context) {
+//   const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=9b0b141b44e2b1a77e4965a9ac513ca4')
+//   const data = await res.json()
+//   return {
+//     props: { data:data.results }
+//   }
+// }
+export async function getStaticProps() {
+  const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=9b0b141b44e2b1a77e4965a9ac513ca4')
+  const data = await res.json()
+
+  return {
+    props: {
+      data:data.results,
+    },
+  }
+}
+
     // useEffect(() => {
     //   onAuthStateChanged(auth, (user) => {
     //   if (user) {
