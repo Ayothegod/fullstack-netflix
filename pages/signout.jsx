ad41cd43ece85cbcd3c929d5f8742ad54cdc287d
Ayo <heyayomideadebisi@gmail.com>
2023-04-11 03:48:44 +0100

    build: build signout page

--- pages/signout.jsx
+++ pages/signout.jsx
@@ -7,6 +7,7 @@ import { useState ,useEffect} from "react";
 const Signout = () => {
       const router = useRouter();
     const [email,setEmail] = useState("")
+    const [date,setDate] = useState("")
 
   const signout = () => {
     signOut(auth)
@@ -23,8 +24,10 @@ const Signout = () => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         const email = user.email
-        console.log(user);
+        const metadata = user.metadata.creationTime
+        console.log({email,metadata});
         setEmail(email)
+        setDate(metadata)
       } else {
         console.log("no user");
       }
@@ -37,18 +40,22 @@ const Signout = () => {
   return (
     <>
     {email ?
-    <div>
-      <div>
+    <div className="bg-black h-screen text-white p-4 flex items-center justify-center  ">
+      <div className="bg-white text-black p-8 rounded-md text-center">
         <div>
-          <p>User Email: {email}</p>
+          <p>User Email : {email}</p>
+          <p>Date Created : {date}</p>
         </div>
        <button className="m-4 w-48 p-3 rounded-md  outline-none text-white border-none bg-red-600 font-semibold text-lg mt-8" onClick={signout}>sign Out</button>
       </div>
      </div> :
-      <div>
+      <div className="bg-black h-screen text-white p-4 flex items-center justify-center  ">
+        <div className="bg-white text-black p-8 rounded-md text-center">
+
         <Link href='/login'>
        <button className="m-4 w-48 p-3 rounded-md  outline-none text-white border-none bg-red-600 font-semibold text-lg mt-8" >sign In</button>
         </Link>
+        </div>
      </div> 
     
   }
