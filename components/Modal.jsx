dc391f15a1607f5a70e7a15e81eb25503609e02c
Ayo <heyayomideadebisi@gmail.com>
2023-04-24 14:46:43 +0100

    add modal width on all screen

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -6,8 +6,8 @@ const Modal = ({data}) => {
   const modal = useModal((state) => state.open);
   const changeModal = useModal((state) => state.changeOpen);
   return (
-    <div className="h-full fixed inset-0 backdrop-blur-sm text-white flex items-center justify-center">
-      <div className="bg-[#fefefe] text-black w-60 h-60 ">
+    <div className="h-full fixed inset-0 backdrop-blur-lg text-white flex items-center justify-center p-4">
+      <div className="bg-[#151414] rounded-md p-4 text-[#999] w-full sm:w-3/4 md:w-1/2 ">
       <p>{data.title}</p>
 
       </div>
