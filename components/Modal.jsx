8b4e22fa8576faa5740114af67dac1b6a6ff9d1a
Ayo <heyayomideadebisi@gmail.com>
2023-04-24 22:17:48 +0100

    add image on modal

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -10,12 +10,14 @@ const Modal = ({ data, setOpen, open }) => {
   return (
     <div className="h-full fixed inset-0 backdrop-blur-sm text-white flex items-center justify-center p-4">
       <div className="bg-[#151414] rounded-md p-4 text-[#999] w-full sm:w-3/4 md:w-1/2 ">
-        <div className="w-48 h-24 relative">
+        <div className="w-full h-60 relative object-contain">
           <Image
             src={imgPath + data.poster_path}
             alt={data.original_title}
-            className=" object-cover object-center  rounded-md hover:scale-110 hover:mx-1 transition-transform duration-300 ease-in-out absolute"
+            className=" object-cover object-center  rounded-md  "
             fill
+            placeholder="blur"
+            blurDataURL="https://placehold.co"
           />
         </div>
 
