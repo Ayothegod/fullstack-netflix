1601b1e116bfc77b44c9fb815f7736c682c2c5a2
Ayo <heyayomideadebisi@gmail.com>
2023-04-24 22:54:49 +0100

    add close button and its func

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import MuiModal from "@mui/material/Modal";
 import Image from "next/image";
+import {GrClose} from "react-icons/gr"
+import {AiOutlineClose} from "react-icons/ai"
+
+
 
 const Modal = ({ data, setOpen, open }) => {
   const imgPath = "http://image.tmdb.org/t/p/w500";
@@ -9,7 +13,10 @@ const Modal = ({ data, setOpen, open }) => {
 
   return (
     <div className="h-full fixed inset-0 backdrop-blur-sm text-white flex items-center justify-center px-8">
-      <div className="bg-[#151414] rounded-md p-2 text-[#999] w-full sm:w-3/4 md:w-1/2 ">
+      <div className="bg-[#151414] rounded-md p-2 text-[#999] w-full sm:w-3/4 md:w-1/2 relative ">
+        <div className="absolute top-3 right-3 bg-neutral-900 p-2 rounded-full z-50 cursor-pointer" onClick={() => setOpen(!open)}>
+          <AiOutlineClose className="text-white text-2xl"/>
+        </div>
         <div className="w-full h-48 sm:h-60 md:h-72 relative">
           <Image
             src={imgPath + data.poster_path}
@@ -21,7 +28,6 @@ const Modal = ({ data, setOpen, open }) => {
           />
         </div>
         <div>
-
         <p>{data.title}</p>
         <p>{data.overview.slice(0,180) }...</p>
         {/* <button>see more</button> */}
