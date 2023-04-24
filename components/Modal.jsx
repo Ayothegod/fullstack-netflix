b1e3715dae4630ebaafc82ca9f14b42785e47665
Ayo <heyayomideadebisi@gmail.com>
2023-04-24 14:32:01 +0100

    add modal bg

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -6,8 +6,8 @@ const Modal = ({data}) => {
   const modal = useModal((state) => state.open);
   const changeModal = useModal((state) => state.changeOpen);
   return (
-    <div className="h-full absolute top-0 left-0 backdrop-blur-xl text-white">
-      <div className="bg-black ">
+    <div className="h-full fixed inset-0 bg-[rgba(0,0,0,0.4)] text-white flex items-center justify-center">
+      <div className="bg-[#fefefe] text-black w-60 h-60 ">
       <p>{data.title}</p>
 
       </div>
