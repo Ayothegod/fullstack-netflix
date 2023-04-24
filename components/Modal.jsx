ddce93457afcd9c42d7c323533eb9c63e2a3ad73
Ayo <heyayomideadebisi@gmail.com>
2023-04-24 14:36:35 +0100

    add modal backdrop blur

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({data}) => {
   const modal = useModal((state) => state.open);
   const changeModal = useModal((state) => state.changeOpen);
   return (
-    <div className="h-full fixed inset-0 bg-[rgba(0,0,0,0.4)] text-white flex items-center justify-center">
+    <div className="h-full fixed inset-0 backdrop-blur-sm text-white flex items-center justify-center">
       <div className="bg-[#fefefe] text-black w-60 h-60 ">
       <p>{data.title}</p>
 
