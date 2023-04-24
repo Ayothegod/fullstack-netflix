36cb99378eb439e9dd0f9fa7384fc6f5795b52d4
Ayo <heyayomideadebisi@gmail.com>
2023-04-24 23:07:03 +0100

    add modal release date

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import MuiModal from "@mui/material/Modal";
 import Image from "next/image";
-import {GrClose} from "react-icons/gr"
-import {AiOutlineClose} from "react-icons/ai"
-
-
+import { GrClose } from "react-icons/gr";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Modal = ({ data, setOpen, open }) => {
   const imgPath = "http://image.tmdb.org/t/p/w500";
@@ -13,9 +11,12 @@ const Modal = ({ data, setOpen, open }) => {
 
   return (
     <div className="h-full fixed inset-0 backdrop-blur-sm text-white flex items-center justify-center px-8">
-      <div className="bg-[#151414] rounded-md p-2 text-[#999] w-full sm:w-3/4 md:w-1/2 relative ">
-        <div className="absolute top-3 right-3 bg-neutral-900 p-2 rounded-full z-50 cursor-pointer" onClick={() => setOpen(!open)}>
-          <AiOutlineClose className="text-white text-2xl"/>
+      <div className="bg-[#151414] rounded-md p-2 text-[#999] w-full sm:w-3/4 md:w-2/3 relative ">
+        <div
+          className="absolute top-3 right-3 bg-neutral-900 p-2 rounded-full z-50 cursor-pointer"
+          onClick={() => setOpen(!open)}
+        >
+          <AiOutlineClose className="text-white text-2xl" />
         </div>
         <div className="w-full h-48 sm:h-60 md:h-72 relative">
           <Image
@@ -27,16 +28,18 @@ const Modal = ({ data, setOpen, open }) => {
             blurDataURL="https://placehold.co"
           />
         </div>
-        <div className="mb-2 mt-4 " >
-        <p className="text-white font-semibold text-lg md:text-xl ">{data.title}</p>
-        <p className="text-sm ">{data.overview.slice(0,180) }...</p>
+        <div className="mb-2 mt-4  ">
+          <div className="flex flex-col gap-y-2">
+            <p className="text-white font-semibold text-lg md:text-xl ">
+            {data.title}
+          </p>
+          <p className="text-sm ">{data.overview.slice(0, 180)}...</p>
+            <p className="text-white text-xs">{data.release_date} <span className="border border-neutral-600 rounded-sm text-sm px-1">HD</span></p>
+          </div>
+          <div>
+
+          </div>
         </div>
-        {/* <button
-          onClick={() => setOpen(!open)}
-          className="bg-red-600 p-2 rounded-md"
-          >
-          Close Modal
-        </button> */}
       </div>
     </div>
   );
