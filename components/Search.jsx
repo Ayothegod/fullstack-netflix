8035345541d01dfee9f4b9d88dadee276ec9cd09
Ayo <heyayomideadebisi@gmail.com>
2023-04-25 15:19:10 +0100

    add aspect-ratio to plugin

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,22 +1,28 @@
-import Image from "next/image"
-import IndMovie from "./IndMovie"
+import Image from "next/image";
+import IndMovie from "./IndMovie";
 
+const Search = ({ searchData }) => {
+  const imgPath = "http://image.tmdb.org/t/p/w500";
+  const secUrl = "https://via.placeholder.com/600x400/cccccc/242020";
 
-const Search = ({searchData}) => {
   return (
     <div className="fixed top-10 p-4">
-        <p>Search Results</p>
-        <div className=" grid grid-col-1 sm:grid-col-3 md:grid-cols-4 gap-4">
-            {
-                searchData.map(data => (
-                    <div key={data.title}>
-                        {/* <Image/> */}
-                    </div>
-                ))
-            }
-        </div>
+      <p>Search Results</p>
+      <div className=" grid grid-col-1 sm:grid-col-3 md:grid-cols-4 gap-4">
+        {searchData.map((data) => (
+          <div key={data.title} className="">
+            <Image
+              src={imgPath + data.poster_path}
+              alt={data.title}
+              fill
+              placeholder="blur"
+              blurDataURL="https://placehold.co"
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
--- tailwind.config.js
+++ tailwind.config.js
@@ -37,5 +37,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
