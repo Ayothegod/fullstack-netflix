bd968448fc183a03694403c71ea34c7c09750239
Ayo <heyayomideadebisi@gmail.com>
2023-04-25 13:18:53 +0100

    add fav description

--- components/Favourites.jsx
+++ components/Favourites.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Modal from "./Modal";
 import { useStore } from "@/lib/store";
 import IndMovie from "./IndMovie";
+import { MdArrowForward } from "react-icons/md";
 
 const Favourites = () => {
   const [open, setOpen] = useState(false);
@@ -13,7 +14,13 @@ const Favourites = () => {
 
   return (
     <div>
-        <p className="text-lg">Favourites <span>click on a </span></p>
+      <p className="text-xl flex items-center gap-2">
+        Favourites{" "}
+        <span className="flex items-center gap-2 text-[#999] text-sm">
+          <MdArrowForward className="mt-1"/>
+          click on any movie card to add to favourites{" "}
+        </span>
+      </p>
       <div className="scroller flex gap-2 overflow-x-scroll overflow-y-hidden ">
         {favourites.map((data, index) => (
           <>
